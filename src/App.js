3f127a0691002052fab9f6e9edac984357db64d4
ui-cod-tv <uicodetv@googlemail.com>
2021-04-30 14:50:56 +0100

    rev 2 - chartjs

--- src/App.js
+++ src/App.js
@@ -1,68 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import { Line } from 'react-chartjs-2';
 
-const data = [
-  {
-    name: 'January',
-    Iphone: 4000
+const data = {
+  labels: ['Jan', 'Mar', 'May', 'July', 'Oct'],
+  datasets: [
+    {
+      label: 'Iphone sales',
+      data: [400, 1000, 4000, 800, 1500],
+      fill: true,
+      backgroundColor:"#2e4355",
+      pointBorderColor:"#8884d8",
+      pointBorderWidth:5,
+      pointRadius:8,
+      tension: 0.4
+    },
+  ],
+};
+
+const options = {
+  plugins:{legend:{display:false}},
+  layout:{padding:{bottom:100}},
+  scales: {
+    y:{
+      ticks:{
+        color:"white",
+        font:{
+          size:18
+        }
+      },
+      grid:{
+        color:"#243240"
+      }
+    },
+    x:{
+      ticks:{
+        color:"white",
+        font:{
+          size:18
+        }
+      }
+    }
   },
-  {
-    name: "March",
-    Iphone: 1000,
-  },
-  {
-    name: "May",
-    Iphone: 4000,
-  },
-  {
-    name: "July",
-    Iphone: 800,
-  },
-  {
-    name: "October",
-    Iphone: 1500,
-  },
-];
+};
 
 function App() {
   return (
-    <>
-      <h2>Quarterly sales figures for mobile phones</h2>
-      <ResponsiveContainer width="90%" aspect={3}>
-      <LineChart
-        width={500}
-        height={300}
-        data={data}
-        margin={{
-          top: 15,
-          right: 30,
-          left: 20,
-          bottom: 5,
-        }}
-      >
-        <CartesianGrid  horizontal="true" vertical="" stroke="#243240"/>
-        <XAxis dataKey="name" tick={{fill:"#fff"}}/>
-        <YAxis tick={{fill:"#fff"}} />
-        <Tooltip contentStyle={{ backgroundColor: "#8884d8", color: "#fff" }} itemStyle={{ color: "#fff" }} cursor={false}/>
-        <Line type="monotone" dataKey="Iphone" stroke="#8884d8" strokeWidth="5" dot={{fill:"#2e4355",stroke:"#8884d8",strokeWidth: 2,r:5}} activeDot={{fill:"#2e4355",stroke:"#8884d8",strokeWidth: 5,r:10}} />
-        
-      </LineChart>
-    </ResponsiveContainer>
-  </>
+    <div className="App">
+      <h2>Quarterly sales for mobile phones</h2>
+      <Line data={data} options={options}/>
+    </div>
   );
 }
 
 export default App;
-
-// background color: #132737; (Goes on the index.css)
-
-// text color: #fff (white)
-
-// Horizontal Grid Color: #243240
-
-// chart line color : #2e4355
- 
-// Data point Dot Fill color : #2e4355  
-
-// Data point Dot Stroke  color (dot outline outline) : #8884d8 
